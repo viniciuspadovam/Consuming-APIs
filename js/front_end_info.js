19c432df31fa369908198dbd48214fe3585005d5
viniciuspadovam <padovam3373@gmail.com>
2021-02-11 20:55:27 -0300

    Refactor a few variable names

--- js/front_end_info.js
+++ js/front_end_info.js
@@ -1,57 +1,70 @@
 async function getInitialData() {
-    try {
-        const res = await fetch('http://localhost:4567/');
+  try {
+    const res = await fetch("http://localhost:4567/");
 
-        const data = await res.json();
+    const data = await res.json();
 
-        showData(data);
-    } catch (e) {
-        console.error(e);
-    }
+    console.log(data);
+    showData(data);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 getInitialData();
 
-const btnConsulta = document.querySelector('#btnConsulta').addEventListener('click', () => {
-    const select = document.querySelector('#selectRegion');
+const btnConsulta = document
+  .querySelector("#btnConsulta")
+  .addEventListener("click", () => {
+    const select = document.querySelector("#selectRegion");
     const value = select.options[select.selectedIndex].value;
 
     (async function getSelectedData() {
-        try {
-            const res = await fetch('http://localhost:4567/selectregion?id=' + value);
+      try {
+        const res = await fetch(
+          "http://localhost:4567/selectregion?id=" + value
+        );
 
-            const data = await res.json();
+        const data = await res.json();
 
-            clearData();
-            showData(data);
-        } catch (e) {
-            console.error(e);
-        }
+        clearData();
+        showData(data);
+      } catch (e) {
+        console.error(e);
+      }
     })();
-});
+  });
 
-function showData(content) {
-    document.querySelector('#city').innerHTML = content.city;
-    document.querySelector('#date').innerHTML = content.date;
-    document.querySelector('#description').innerHTML = content.description;
-    document.querySelector('#humidity').innerHTML = `Humidade: ${content.humidity} kg/m³`;
-    document.querySelector('#temp').innerHTML = `Temperatura: ${content.temp} ºC`;
+const label_city = document.querySelector("#city");
+const label_date = document.querySelector("#date");
+const label_description = document.querySelector("#description");
+const label_humidity = document.querySelector("#humidity");
+const label_temperature = document.querySelector("#temp");
 
-    for (let i = 1; i <= 4; i++) {
-        document.querySelectorAll('.forecast')[i - 1].innerHTML = `${content.forecast[i].weekday} - 
-        ${content.forecast[i].date} <br /> ${content.forecast[i].description} <br /> 
-        Temp. Min.: ${content.forecast[i].min} - Temp. Max.: ${content.forecast[i].max}`;
-    }
+function showData({ city, date, description, humidity, temp, forecast }) {
+  label_city.innerHTML = city;
+  label_date.innerHTML = date;
+  label_description.innerHTML = description;
+  label_humidity.innerHTML = `Humidade: ${humidity} kg/m³`;
+  label_temperature.innerHTML = `Temperatura: ${temp} ºC`;
+
+  for (let i = 1; i <= 4; i++) {
+    document.querySelectorAll(".forecast")[
+      i - 1
+    ].innerHTML = `${forecast[i].weekday} - 
+        ${forecast[i].date} <br /> ${forecast[i].description} <br /> 
+        Temp. Min.: ${forecast[i].min} - Temp. Max.: ${forecast[i].max}`;
+  }
 }
 
 function clearData() {
-    document.querySelector('#city').innerHTML = '';
-    document.querySelector('#date').innerHTML = '';
-    document.querySelector('#description').innerHTML = '';
-    document.querySelector('#humidity').innerHTML = '';
-    document.querySelector('#temp').innerHTML = '';
+  label_city.innerHTML = "";
+  label_date.innerHTML = "";
+  label_description.innerHTML = "";
+  label_humidity.innerHTML = "";
+  label_temperature.innerHTML = "";
 
-    for (let i = 1; i <= 4; i++) {
-        document.querySelectorAll('.forecast')[i - 1].innerHTML = '';
-    }
+  for (let i = 1; i <= 4; i++) {
+    document.querySelectorAll(".forecast")[i - 1].innerHTML = "";
+  }
 }
